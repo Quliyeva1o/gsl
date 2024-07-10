8e5bc651ecb0c9ca4a9da6efe9c588efc851d55c
Quliyeva1o <renaquliyeva646@gmail.com>
2024-07-10 18:28:45 +0400

    .

--- front/src/Layout/Header/index.jsx
+++ front/src/Layout/Header/index.jsx
@@ -30,6 +30,7 @@ const Header = () => {
     if (examSection) {
       const examSectionPosition = examSection.offsetTop - 100; 
       window.scrollTo({ top: examSectionPosition, behavior: 'smooth' });
+     
     }
   };
 
